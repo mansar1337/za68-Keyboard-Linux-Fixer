2c2837e8a87fae32f33fff4504d9b82b1d3a74f5
mansar1337 <ivan.petr.cool@gmail.com>
2024-12-28 17:22:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ echo "za68fix install"
 chmod +x za68fix
 sudo cp za68fix /usr/local/bin/
 echo "za68fix installed!"
+echo "you can use fix: sudo za68fix"
+echo "wait 10s for auto run za68fix..."
+sleep 10
 clear
 sudo za68fix
