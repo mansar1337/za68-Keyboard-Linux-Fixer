4ef3d8f1792d47a40dffadfe0b61230d59b7b812
mansar1337 <ivan.petr.cool@gmail.com>
2024-12-28 18:41:36 +0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-echo "remove fix"
-sudo rm /usr/local/bin/za68fix
-sudo rm /usr/local/bin/restore_GRUB.sh
-echo "fix deleted"
-echo "delete the repo folder"
+
+echo "Starting cleanup process..."
+
+# Remove fix-related files
+echo "Removing fix files..."
+if [ -f /usr/local/bin/za68fix ]; then
+    sudo rm /usr/local/bin/za68fix
+    echo "- za68fix removed"
+else
+    echo "- za68fix not found"
+fi
+
+if [ -f /usr/local/bin/restore_GRUB.sh ]; then
+    sudo rm /usr/local/bin/restore_GRUB.sh
+    echo "- restore_GRUB.sh removed"
+else
+    echo "- restore_GRUB.sh not found"
+fi
+
+echo "Cleanup completed successfully"
